1d321f336c9113bd6687c686f4f45e1308a40541
Giovanna Bellonia <giovannabellonia@gmail.com>
2021-03-22 16:55:41 -0300

    atualizacao

--- prog2/client.py
+++ prog2/client.py
@@ -8,16 +8,17 @@ def run():
     ip_adress = sys.argv[1]
     service_name = sys.argv[2]
 
-    channel = grpc.insecure_channel(ip_adress)
+    # channel = grpc.insecure_channel(ip_adress)
+    channel = grpc.insecure_channel('localhost:8888')
     stub = services_pb2_grpc.DoStuffStub(channel)
 
-    # my_pid = os.getpid()
+    my_pid = os.getpid()
 
-    # response = stub.say_hello(services_pb2.HelloRequest(pid=my_pid))
-    # print("GRPC client received: " + response.retval)
+    response = stub.say_hello(services_pb2.HelloRequest(pid=my_pid))
+    print("GRPC client received: " + response.retval)
 
-    # response = stub.say_hello_again(services_pb2.HelloRequest(pid=my_pid))
-    # print("GRPC client received: " + response.retval)
+    response = stub.say_hello_again(services_pb2.HelloRequest(pid=my_pid))
+    print("GRPC client received: " + response.retval)
 
     response = stub.get_service_port(services_pb2.ServiceName(name=service_name))
     print("GRPC client received: " + str(response.port))
