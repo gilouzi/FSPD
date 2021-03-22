56293815d0f805ebfc0e47875a858d9ee34f0b10
Giovanna Bellonia <giovannabellonia@gmail.com>
2021-03-22 17:07:59 -0300

    atualizacao

--- prog2/server.py
+++ prog2/server.py
@@ -16,15 +16,15 @@ class DoStuff(services_pb2_grpc.DoStuffServicer):
 
     def get_service_port(self, request, context):
         print(1)
-        service_port = services_dict[request.serviceName]['port'] if request.service_name in services_dict.keys() else -1
+        service_port = services_dict[request.name]['port'] if request.name in services_dict.keys() else -1
         print(2)
-        print("GRPC server in get_service_port, adress =", str(context.peer()), "service name =", str(request.serviceName), "port =", service_port)
+        print("GRPC server in get_service_port, adress =", str(context.peer()), "service name =", str(request.name), "port =", service_port)
         print(3)
         return services_pb2.ServicePort(port=service_port)
     
     def get_service_description(self, request, context):
-        service_description = services_dict[request.serviceName]['description'] if request.service_name in services_dict.keys() else -1
-        print("GRPC server in get_service_port, adress =", str(context.peer()), "service name =", str(request.serviceName), "description =", service_description)
+        service_description = services_dict[request.name]['description'] if request.name in services_dict.keys() else -1
+        print("GRPC server in get_service_port, adress =", str(context.peer()), "service name =", str(request.name), "description =", service_description)
         return services_pb2.ServiceDescription(description=service_description)
 
 def serve():
