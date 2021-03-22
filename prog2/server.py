7ac242e4eebd0d888dcfa376ba99521e8ba5c215
Giovanna Bellonia <giovannabellonia@gmail.com>
2021-03-22 18:28:05 -0300

    atualizacao

--- prog2/server.py
+++ prog2/server.py
@@ -16,12 +16,18 @@ class DoStuff(services_pb2_grpc.DoStuffServicer):
 
     def get_service_port(self, request, context):
         service_port = services_dict[request.name]['port'] if request.name in services_dict.keys() else -1
-        print("GRPC server in get_service_port, adress =", str(context.peer()), "service name =", str(request.name), "port =", str(service_port))
+        print("client adress =", str(context.peer()), 
+                "command called = get_service_port ",
+                "parameter =", request.name, 
+                "answer =", str(service_port))
         return services_pb2.ServicePort(port=service_port)
     
     def get_service_description(self, request, context):
         service_description = services_dict[request.name]['description'] if request.name in services_dict.keys() else ""
-        print("GRPC server in get_service_port, adress =", str(context.peer()), "service name =", str(request.name), "description =", service_description)
+         print("client adress =", str(context.peer()), 
+                "command called = get_service_description ",
+                "parameter =", request.name, 
+                "answer =", service_description)
         return services_pb2.ServiceDescription(description=service_description)
 
 def serve():
@@ -30,11 +36,12 @@ def serve():
         arq = f.read().splitlines()
 
     for line in arq:
-        if line[0] != '#':
+        if line != '' and line[0] != '#':
             line = line.split('/')
             service, port = line[0].split()
             description = line[1]
-            services_dict[service] = {'port': int(port), 'description': description}
+            if service not in services_dict.keys():
+                services_dict[service] = {'port': int(port), 'description': description}
 
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     services_pb2_grpc.add_DoStuffServicer_to_server(DoStuff(), server)
