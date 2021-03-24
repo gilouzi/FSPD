0bfb4ac297d936d95a2481878a585d8314e41e4f
Giovanna Bellonia <giovannabellonia@gmail.com>
2021-03-24 15:45:30 -0300

    atualizacao

--- prog2/server.py
+++ prog2/server.py
@@ -3,7 +3,6 @@ import grpc
 import sys
 import services_pb2, services_pb2_grpc
 
-services_dict = dict()
 class DoStuff(services_pb2_grpc.DoStuffServicer):
     def __init__(self, txt):
         self.services_dict = dict()
@@ -22,21 +21,21 @@ class DoStuff(services_pb2_grpc.DoStuffServicer):
                 description = description[0]
                 description = description.split()
                 
-                if service not in services_dict.keys():
-                    services_dict[service] = {'port': int(port), 'protocol': description[0]}
+                if service not in self.services_dict.keys():
+                    self.services_dict[service] = {'port': int(port), 'protocol': description[0]}
                     
                     if len(description) > 1:
-                        services_dict[service]['aliases'] = ', '.join(description[1:])
+                        self.services_dict[service]['aliases'] = ', '.join(description[1:])
                     else:
-                        services_dict[service]['aliases'] = ''
+                        self.services_dict[service]['aliases'] = ''
 
                     if comments:
-                        services_dict[service]['comments'] = comments
+                        self.services_dict[service]['comments'] = comments
                     else:
-                        services_dict[service]['comments'] = ''
+                        self.services_dict[service]['comments'] = ''
                         
     def get_service_port(self, request, context):
-        service_port = services_dict[request.name]['port'] if request.name in services_dict.keys() else -1
+        service_port = self.services_dict[request.name]['port'] if request.name in self.services_dict.keys() else -1
         print("client adress =", str(context.peer()), 
                 "| command called = get_service_port ",
                 "| service name =", request.name, 
@@ -44,16 +43,16 @@ class DoStuff(services_pb2_grpc.DoStuffServicer):
         return services_pb2.ServicePort(port=service_port)
     
     def get_service_description(self, request, context):
-        if request.name in services_dict.keys():
+        if request.name in self.services_dict.keys():
             print("client adress =", str(context.peer()), 
                     "| command called = get_service_description ",
                     "| service name =", request.name, 
-                    "| protocol =", services_dict[request.name]['protocol'],
-                    "| aliases =", services_dict[request.name]['aliases'],
-                    "| comments =", services_dict[request.name]['comments'])
-            return services_pb2.ServiceDescription(protocol=services_dict[request.name]['protocol'], 
-                                                aliases=services_dict[request.name]['aliases'],
-                                                comments=services_dict[request.name]['comments'])
+                    "| protocol =", self.services_dict[request.name]['protocol'],
+                    "| aliases =", self.services_dict[request.name]['aliases'],
+                    "| comments =", self.services_dict[request.name]['comments'])
+            return services_pb2.ServiceDescription(protocol=self.services_dict[request.name]['protocol'], 
+                                                aliases=self.services_dict[request.name]['aliases'],
+                                                comments=self.services_dict[request.name]['comments'])
         else:
             print("client adress =", str(context.peer()), 
                     "| command called = get_service_description ",
